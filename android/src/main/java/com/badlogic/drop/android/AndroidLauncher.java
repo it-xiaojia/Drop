b0753fbb7cb40e0bfb406f28ef95232da411f6aa
高佳 <1598298853@qq.com>
2025-02-20 13:56:38 +0800

    安卓端更新到新版启动，添加开发者信息

--- android/src/main/java/com/badlogic/drop/android/AndroidLauncher.java
+++ android/src/main/java/com/badlogic/drop/android/AndroidLauncher.java
@@ -2,9 +2,9 @@ package com.badlogic.drop.android;
 
 import android.os.Bundle;
 
+import com.badlogic.drop.Drop;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.badlogic.drop.Main;
 
 /** Launches the Android application. */
 public class AndroidLauncher extends AndroidApplication {
@@ -13,6 +13,6 @@ public class AndroidLauncher extends AndroidApplication {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
         configuration.useImmersiveMode = true; // Recommended, but not required.
-        initialize(new Main(), configuration);
+        initialize(new Drop(), configuration);
     }
 }
--- core/src/main/java/com/badlogic/drop/MainMenuScreen.java
+++ core/src/main/java/com/badlogic/drop/MainMenuScreen.java
@@ -35,10 +35,11 @@ public class MainMenuScreen implements Screen {
         game.batch.draw(game.backgroundTexture, 0, 0, game.viewport.getWorldWidth(), game.viewport.getWorldHeight());
         game.font.setColor(Color.RED);
         game.font.getData().setScale(0.015f);
-        game.font.draw(game.batch, "欢迎来到Drop！！！", 1, 1.5f);
+        game.font.draw(game.batch, "欢迎来到Drop！！！", 2.5f, 3);
         game.font.setColor(Color.WHITE);
         game.font.getData().setScale(game.viewport.getWorldHeight() / Gdx.graphics.getHeight());
-        game.font.draw(game.batch, "点击任意位置开始游戏！", 1, 1);
+        game.font.draw(game.batch, "点击任意位置开始游戏！", 2.7f, 2);
+        game.font.draw(game.batch, "二次开发作者：IT小佳", 5.5f, 0.5f);
 
         game.batch.end();
 
